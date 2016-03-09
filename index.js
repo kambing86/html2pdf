224ce8ab6dc622a42c8570980c6fc84c98a462e3
Chua Kang Ming <kangming.chua@3radical.com>
2016-03-09 17:12:43 +0800

    add gzip compression add checking for id add archive for backup in server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const pdf = require('html-pdf');
 const express = require('express');
-const bodyParser = require('body-parser')
+const compression = require('compression');
+const bodyParser = require('body-parser');
 const fs = require('fs');
+const xz = require('xz');
 const moment = require('moment');
 
 const app = express();
@@ -17,17 +19,24 @@ if (!fs.existsSync(backupPath))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
   extended: false
-}))
+}));
+app.use(compression());
 
 app.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/test.html');
 });
 
 app.post('/pdf', function(req, res, next) {
+  if (!(!!req.body.id))
+  {
+    res.end("Please provide id");
+    return;
+  }
   res.set('Content-Type', 'application/pdf');
   pdf.create(req.body.html, paperFormat).toStream(function(err, stream) {
-    stream.pipe(fs.createWriteStream(backupPath + '/' + req.body.id + '_' +
-      moment().format("YYYYMMDDHHmmss") + '.pdf'))
+    var filename = backupPath + '/' + req.body.id + '_' + moment().format("YYYYMMDDHHmmss") + '.pdf.xz';
+    var compressor = new xz.Compressor(9);
+    stream.pipe(compressor).pipe(fs.createWriteStream(filename));
     stream.pipe(res);
   });
 });
