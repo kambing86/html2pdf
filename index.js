ab743a85581d9c8b1cb44826a411d3dc88ccc5d5
Chua Kang Ming <kangming.chua@3radical.com>
2016-03-09 16:17:12 +0800

    add device id and save to server for backup

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const pdf = require('html-pdf');
 const express = require('express');
 const bodyParser = require('body-parser')
+const fs = require('fs');
+const moment = require('moment');
+
 const app = express();
 const paperFormat = {
   "format": "A4",
   "orientation": "portrait"
 };
+const backupPath = __dirname + '/bak';
+
+if (!fs.existsSync(backupPath))
+  fs.mkdirSync(backupPath);
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 app.get('/', function(req, res, next) {
   res.sendFile(__dirname + '/test.html');
@@ -17,6 +26,8 @@ app.get('/', function(req, res, next) {
 app.post('/pdf', function(req, res, next) {
   res.set('Content-Type', 'application/pdf');
   pdf.create(req.body.html, paperFormat).toStream(function(err, stream) {
+    stream.pipe(fs.createWriteStream(backupPath + '/' + req.body.id + '_' +
+      moment().format("YYYYMMDDHHmmss") + '.pdf'))
     stream.pipe(res);
   });
 });
